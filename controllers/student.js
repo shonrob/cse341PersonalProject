40d080b78a1290f6e4b0a24e08a95fdc5c406fcf
shonrob <rob19063@byui.edu>
2023-02-06 12:55:21 -0700

    have post working but not my rest file

--- controllers/student.js
+++ controllers/student.js
@@ -29,13 +29,13 @@ const getStudentById = async (request, response) => {
 };
 
 const createStudent = async (req, res, next) => {
-    console.log(req.body);
+    // console.log(req.body);
     try {
-        const student = new student(req.body);
-        
+        const student = new Student(req.body);
+
         try {
             await student.save();
-            console.log("hello");  
+            // console.log("hello");  
             console.log(student);         
         } catch (error) {
             setHeaders(res, contentText);
@@ -54,95 +54,7 @@ const createStudent = async (req, res, next) => {
 }
 
 module.exports = {getAllStudents, getStudentById, createStudent}
-// exports.createStudent( async (request, response) => {
-//     student = new studentModel(request.body);
-//         try {
-//             await student.save();
-//             response.send(student);
-//         } catch (error) {
-//             response.status(500).send(error);
-//         }
-// }); 
 
-// exports.getAllStudents = (req, res) => {
-//     if (req.header('') === ){
-//         student.find({}, {
-//             firstName: 1,
-//             lastName: 1,
-//             enrolled: 1,
-//             _id: 0
-//         }).then((data) =>{
-//             res.send(data);
-//         })
-//         .catch((err) => {
-//             res.status(500).send({
-//                 message:
-//                     err.message || 'An error when retriving student.',
-//             });
-//         });
-//     } else {
-//         res.send('Invalid apiKey.');
-//     }
-// };
-
-// exports.getAllStudents = (req, res) => {
-//     student.find({}).then((data) => {
-//         res.send(data);
-//     })
-//     .catch((err) => {
-//         res.status(500).send({
-//             message: err.message || 'An error occurred while retriving the students.'
-//         });
-//     });
-// };
-
-
-
-
-
-
-
-// function getAllStudents (request, response) {
-//     student.find({}).then, (err, result) => {
-//         if (err) {
-//             return (err);
-//         }else {
-//             response.setHeader("Content-Type", "application/json");
-//             response.status(200).json(result);
-//         }
-//     }
-        
-//         // result.toArray().then(list => {
-
-//     // });
-// }
-
-// const getStudentById = async (request, response) => {
-//     const studentId = new ObjectId(request.params.id);
-// //     // getting the information from the mongo db site 
-//     const result = await student.find({ _id: studentId});
-    
-
-//     result.toArray().then(list => {
-//         response.setHeader("Content-Type", "application/json")
-//         response.status(200).json(list);
-//     });
-
-// };
-
-// // a function that will use POST 
-// async function createStudent (req, res, next) {
-//     console.log(req.body);
-//     const newStudent = new student(req.body);
-//     newStudent.save();
-//     // const response = await mongodb.getDb().db("homework").collection("student").insertOne(req.body);
-//     setHeaders(res);
-//     if (response.acknowledged) {
-//         res.status(201).json(response);    
-//     } else {
-//         res.status(500).json(response.error || "Sorry, error occured when creating student");
-//     }    
-// }
 
 // // a function that will update a student by id through PUT
 // const updateStudent = async (req, res, next) => {
