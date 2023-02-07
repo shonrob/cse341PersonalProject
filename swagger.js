13bf14905aa84bd8263cc98720ea88dd679c40b7
shonrob <rob19063@byui.edu>
2023-02-06 22:42:01 -0700

    swagger js update

--- swagger.js
+++ swagger.js
@@ -34,7 +34,7 @@ const doc = {
 
 
 const outputFile = './swagger-output.json';
-const endpointsFiles = ['server.js'];
+const endpointsFiles = ['./server.js'];
 
 /* NOTE: if you use the express Router, you must pass in the 
    'endpointsFiles' only the root file where the route starts,
